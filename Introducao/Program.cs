8fe98e2e766bf38a230576c308c4267b322c58f4
koddeom <koddeom@gmail.com>
2023-01-25 14:39:29 -0300

    commit inicial

--- Introducao/Program.cs
+++ Introducao/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+var numerofiltrados = numeros.Where(n=> n > 4);   
+
+foreach (int numero in numerofiltrados)
+{
+    Console.WriteLine(numero);
+}
+
+Console.ReadKey();  
